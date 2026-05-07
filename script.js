efb21fcaca3e51339c0708ac2f77c0f4fd5d2d86
ann8th <ann8th@gmail.com>
2026-05-07 18:06:47 +0300

    Write function getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Testing...");
+// Get the computer choice
+
+function getComputerChoice() {
+    let computerChoice;
+    let randomNum = Math.floor(Math.random() * 3 + 1);
+    randomNum === 1 ? computerChoice = "rock" :
+    randomNum === 2 ? computerChoice = "paper" :
+    randomNum === 3 ? computerChoice = "scissors" :
+    computerChoice = randomNum;
+    return computerChoice;
+}
+
+// let i = 0
+//     , rock = 0
+//     , paper = 0
+//     , scissors = 0;
+
+
+// do {
+//     let testVal = getComputerChoice(); 
+//     testVal === "rock" ? rock++ : 
+//     testVal === "paper" ? paper++ :
+//     testVal === "scissors" ? scissors++ : console.log(testVal);
+//     i++;
+// } while (i < 30);
+
+// console.log(`rock=${rock}\npaper=${paper}\nscissors=${scissors}\ni=${i}`);
+
